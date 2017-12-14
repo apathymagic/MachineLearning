348798fced3bbd9af6a0660ff0e592ab3694b4d0
apathymagic <apathymagic@163.com>
2017-12-14 21:50:07 +0800

    tensorboard

--- Tensorflow_layer.py
+++ Tensorflow_layer.py
@@ -4,28 +4,38 @@ import matplotlib.pyplot as plt
 
 
 def add_layer(inputs, in_size, out_size, activation_function=None):
-    Weights = tf.Variable(tf.random_normal([in_size, out_size]))
-    biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
-    Wx_puls_b = tf.matmul(inputs, Weights) + biases
-    if activation_function is None:
-        outputs = Wx_puls_b
-    else:
-        outputs = activation_function(Wx_puls_b)
-    return outputs
+    # add one more layer and return the output of this layer
+    with tf.name_scope('layer'):
+        with tf.name_scope('weights'):
+            Weights = tf.Variable(tf.random_normal([in_size, out_size]))
+        with tf.name_scope('biases'):
+            biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
+        with tf.name_scope('Wx_puls_b'):
+            Wx_puls_b = tf.matmul(inputs, Weights) + biases
+        if activation_function is None:
+            outputs = Wx_puls_b
+        else:
+            outputs = activation_function(Wx_puls_b)
+        return outputs
 
 
 x_data = np.linspace(-1, 1, 300)[:, np.newaxis]
 noise = np.random.normal(0, 0.05, x_data.shape)
 y_data = np.square(x_data) - 0.5 + noise
 
-xs = tf.placeholder(tf.float32, [None, 1])
-ys = tf.placeholder(tf.float32, [None, 1])
+# define placeholder for inputs to network
+with tf.name_scope('inputs'):
+    xs = tf.placeholder(tf.float32, [None, 1], name='x_input')
+    ys = tf.placeholder(tf.float32, [None, 1], name='y_input')
 
+# add hide layer
 layer1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
 prediction = add_layer(layer1, 10, 1, activation_function=None)
 
-loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction), reduction_indices=[1]))
-train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+with tf.name_scope('loss'):
+    loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction), reduction_indices=[1]))
+with tf.name_scope('train'):
+    train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
@@ -34,6 +44,7 @@ plt.ion()
 plt.show()
 
 with tf.Session() as sess:
+    writer = tf.summary.FileWriter("logs/", sess.graph)
     tf.global_variables_initializer().run()
     for step in range(1000):
         sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
@@ -48,3 +59,4 @@ with tf.Session() as sess:
             prediction_value = sess.run(prediction, feed_dict={xs: x_data, ys: y_data})
             lines = ax.plot(x_data, prediction_value, 'r-', lw=5)
             plt.pause(0.1)
+    plt.pause(10)
