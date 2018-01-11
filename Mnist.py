4846e0e02db37658d44a2901752ef8350abbdcee
apathymagic <apathymagic@163.com>
2018-01-11 21:21:15 +0800

    Mnist-e2

--- Mnist.py
+++ Mnist.py
@@ -28,22 +28,22 @@ xs = tf.placeholder(tf.float32, [None, 784])
 ys = tf.placeholder(tf.float32, [None, 10])
 
 # add layer
-layer1 = add_layer(xs, 784, 500, tf.nn.relu)
-prediction = add_layer(layer1, 500, 10, None)
+# layer1 = add_layer(xs, 784, 500, tf.nn.relu)
+prediction = add_layer(xs, 784, 10, tf.nn.softmax)
 
 # calculate loss
-cross_entropy = tf.nn.softmax_cross_entropy_with_logits(ys, prediction)
-loss = tf.reduce_mean(cross_entropy)
+# cross_entropy = -tf.reduce_sum(ys * tf.log(prediction))
+loss = tf.reduce_mean(-tf.reduce_sum(ys * tf.log(prediction)), reduction_indices=[1])
 
 # optimizer
-train_step = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
 
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 # run
 with tf.session() as sess:
     tf.initialize_all_variables().run()
-    for step in range(10000):
+    for step in range(1000):
         batch_xs, batch_ys = mnist.train.next_batch(100)
         sess.run(train_step, feed_dict={xs: batch_xs, ys: batch_ys})
-        if step % 1000 == 0:
+        if step % 50 == 0:
             print(compute_accuracy(sess, mnist.test.images, mnist.test.labels))
